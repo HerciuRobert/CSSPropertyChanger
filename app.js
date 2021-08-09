4cda96eeb116dd29deb4a385743367e80e2b28b3
RobertH <herciu.robert@gmail.com>
2021-08-09 17:22:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+let blockElement = document.querySelector("#block");
+let verticalPosition = document.querySelector("#position-y");
+let horizontalPosition = document.querySelector("#position-x");
+let blockSize = document.querySelector("#size");
+let shapeSelector = document.querySelector("#shape-select");
+let okBtn = document.querySelector("#ok-btn");
+let rgbaR = document.querySelector("#rgba-r");
+let rgbaG = document.querySelector("#rgba-g");
+let rgbaB = document.querySelector("#rgba-b");
+let rgbaA = document.querySelector("#rgba-a");
+let rgbaContainer = document.querySelector(".rgba-container");
+let rgbaInputs = document.querySelectorAll("input");
+
+// Vertical Position Changer
+
+verticalPosition.addEventListener("change", function() {
+    blockElement.style.top = verticalPosition.value + "px";
+});
+
+// Horizontal Position Changer
+
+horizontalPosition.addEventListener("change", function() {
+    blockElement.style.left = horizontalPosition.value + "px";
+});
+
+// Size Changer
+
+blockSize.addEventListener("change", function() {
+    blockElement.style.transform = "scale(" + blockSize.value + ")";
+});
+
+//Shape Changer
+
+okBtn.addEventListener("click", function() {
+    let shapeOption = shapeSelector.value;
+    if(shapeOption == 1) {
+        blockElement.style.borderRadius = "0"
+    } else if(shapeOption == 2) {
+        blockElement.style.borderRadius = "50%"
+    }
+});
+
+//Background Color Changer 
+
+for(i = 0; i < rgbaInputs.length; i++) {
+    rgbaInputs[i].addEventListener("change", function() {
+        blockElement.style.backgroundColor = "rgba(" + rgbaR.value + "," + rgbaG.value + "," + rgbaB.value + "," + rgbaA.value + ")"
+    })
+}
+
